dbb52d6b959ce2191badfb4497a08d2d7dbf7218
mpantoja314 <31548174+mpantoja314@users.noreply.github.com>
2017-09-01 22:29:57 -0700

    Update ImageTagGUI.py

--- ImageTagGUI.py
+++ ImageTagGUI.py
@@ -11,7 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# ==============================================================================import gtk
+# ==============================================================================
+import gtk
 gtk.set_interactive(False)
 import Tkinter as tk
 from Tkinter import *
